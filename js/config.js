327d32ad94f44feead04002899e08a2be5d2fda1
RKM0930 <rkm093004@gmail.com>
2026-09-16 22:07:09 +0800

    chore: ignore local config and runtime artifacts

--- js/config.js
+++
@@ -1,4 +0,0 @@
-export const SUPABASE_CONFIG = {
-  url: 'https://bgjzmnqqeqfcttjpotwg.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnanptbnFxZXFmY3R0anBvdHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1MTMyODAsImV4cCI6MjEwNTA4OTI4MH0.d_fE0I28hvc5C9ZkJAAdzXzsMTXmQGSthYuiCoYj_rc',
-};
